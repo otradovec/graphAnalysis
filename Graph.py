8bff7349a57475da8216af3c3e988ea83da8a8b5
otradovec <otradovec@github.com>
2021-02-24 13:21:19 +0100

    Added connecting method

--- Graph.py
+++ Graph.py
@@ -2,5 +2,13 @@ class Graph:
 
     def __init__(self,nodes):
         self.nodes = nodes
-    def hasNode(self,nodeName):
+        self.connections = []
+    def has_node(self,nodeName):
         return nodeName in self.nodes
+    def add_not_oriented_connection(self,first_node, second_node):
+        if not self.has_node(first_node) or not self.has_node(second_node):
+            raise ValueError("Adding connection with not  existing node")
+        self.connections.append([first_node,second_node])
+        self.connections.append([second_node,first_node])
+    def is_connection(self,from_node, to_node):
+        return [from_node,to_node] in self.connections
--- GraphTest.py
+++ GraphTest.py
@@ -5,8 +5,18 @@ class GraphTest(unittest.TestCase):
 
     def test_add_node(self):
         g = Graph(["A","B"])
-        self.assertTrue(g.hasNode("A"))
-        
+        self.assertTrue(g.has_node("A"))
+        self.assertFalse(g.has_node("C"))
+
+    def test_add_not_oriented_connection(self):
+        g = Graph(["A","B","C"])
+        g.add_not_oriented_connection("A","B")
+        self.assertTrue(g.is_connection("A","B"))
+        self.assertTrue(g.is_connection("B","A"))
+        self.assertFalse(g.is_connection("A","C"))
+        with self.assertRaises(ValueError):
+             g.add_not_oriented_connection("A","Z")
+
 
 if __name__ == '__main__':
     unittest.main()
