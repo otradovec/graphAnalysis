ec618a7ed97136e71955c999bc6f235580c809bd
otradovec <otradovec@github.com>
2021-02-28 22:58:32 +0100

    cleaning code

--- fusion.py
+++ fusion.py
@@ -3,21 +3,10 @@ input_lines = []
 for line in sys.stdin:
     input_lines.append(line)
 
-#first_stores = input_lines[0]
-#first_stores = "".join(first_stores.split()).split(",")
-#for i in range(0,len(first_stores)):
-#    first_stores[i] = first_stores[i].lower()
-#second_stores = input_lines[1]
-#second_stores = "".join(second_stores.split()).split(",")
-#from Graph import Graph
-#g = Graph(set(first_stores) | set(second_stores))
-
 connections = []
 for i in range(2,len(input_lines)):
-    connection = input_lines[i].replace("\r","").replace("\n","")#.lower()
-#    connection = "".join(connection.split()).split("->")
+    connection = input_lines[i].replace("\r","").replace("\n","")
     connections.append(connection)
-#g.add_oriented_connections(connections)
 
 simpleConnections = []
 redundantConnections = []
